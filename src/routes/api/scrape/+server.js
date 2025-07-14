fb459ed4cafb9ccbc689383f246b4fdb99ffd904
Jeffrey Duivenvoorden <jeffrey.duivenvoorden@allunited.nl>
2025-07-14 13:52:59 +0200

    fix: Scraping should not be executed during chunk prerendering

--- src/routes/api/scrape/+server.js
+++ src/routes/api/scrape/+server.js
@@ -3,21 +3,21 @@ import { beers } from '$lib/server/db/schema';
 import { scrapeBeers } from '$lib/scraper';
 import { eq } from 'drizzle-orm';
 
-const scraped = await scrapeBeers();
+export const GET = async () => {
+	// Insert new beers if not present
+	// todo: i'm glad none else uses this site, if i read the performance issues with this
+	const scraped = await scrapeBeers();
 
-exit;
-
-// Insert new beers if not present
-// todo: i'm glad none else uses this site, if i read the performance issues with this
-for (const beer of scraped) {
-	const existing = await db.select().from(beers).where(eq(beers.link, beer.link));
-	if (!existing.length > 0) {
-		await db.insert(beers).values({
-			title: beer.title,
-			link: beer.link,
-			img: beer.img,
-			price: beer.price,
-			attributes: {}
-		});
+	for (const beer of scraped) {
+		const existing = await db.select().from(beers).where(eq(beers.link, beer.link));
+		if (!existing.length > 0) {
+			await db.insert(beers).values({
+				title: beer.title,
+				link: beer.link,
+				img: beer.img,
+				price: beer.price,
+				attributes: {}
+			});
+		}
 	}
-}
+};
